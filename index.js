c8b57a5906f234fb83b100d97efa465ed3410eb2
Bayejid Hawlader <bayejidhawlader@gmail.com>
2022-11-07 06:05:26 +0600

    Basic mondo db node_Modules setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// User : geniusDbUser
+// Pass : NUcEztlCFjfJ8Zmx
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.athiem3.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get("/", (req, res) => {
+  res.send("genios car server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Geniors car surver running on ${port}`);
+});
